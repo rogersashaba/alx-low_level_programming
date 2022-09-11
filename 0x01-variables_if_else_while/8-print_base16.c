f1f0a6bfd7d53a31a3d53a6a1312bdb64329acf7
Rogers Ashaba <65979430+rogersashaba@users.noreply.github.com>
2022-09-12 00:24:17 +0300

    Numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char p;
+
+	for (p = '0'; p <= '9'; p++)
+	{
+		putchar(p);
+	}
+	for (p = 'a'; p <= 'f'; p++)
+	{
+		putchar(p);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
