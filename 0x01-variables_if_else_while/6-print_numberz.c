eed17e927e479160bef43407c101549f94f82023
Rogers Ashaba <65979430+rogersashaba@users.noreply.github.com>
2022-09-12 00:20:04 +0300

    Single digit numbers base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
