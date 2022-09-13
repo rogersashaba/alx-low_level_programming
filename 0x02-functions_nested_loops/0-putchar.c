3982b47a1c616e19d81b20a0cec6f80d705194e1
Rogers Ashaba <65979430+rogersashaba@users.noreply.github.com>
2022-09-14 01:52:08 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main -Entry point
- *
- * Return: Always 0 (Success)
+ * main - print putchar
+ * Return: always 0
  */
 
 int main(void)
@@ -18,6 +17,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-	return (0);
-
-}	
+return (0);
+}
