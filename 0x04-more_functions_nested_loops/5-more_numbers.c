55233e0d50fa8e322893afb7bd136b9bc768742f
Rogers Ashaba <65979430+rogersashaba@users.noreply.github.com>
2022-09-16 01:22:58 +0300

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* more_numbers - Prints numbers 10 times
+*
+* Return: 0 if successful
+*/
+
+void more_numbers(void)
+{
+	char i;
+	int counter;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i / 10 > 0)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
+
+
