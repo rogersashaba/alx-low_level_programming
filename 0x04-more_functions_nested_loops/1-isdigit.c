e73f29a183716db6a4c7695446e7603c1b5f6365
Rogers Ashaba <65979430+rogersashaba@users.noreply.github.com>
2022-09-16 01:19:17 +0300

    Isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+* _isdigit - Checks if a character is uppercase
+* @c: type char alphabet
+* Return: 1 if uppercase, 0 if not
+*/
+
+int _isdigit(int c)
+{
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
