cd5a65a1cca96c72f7c741a0b007614ef617ea02
Rogers Ashaba <65979430+rogersashaba@users.noreply.github.com>
2022-09-20 04:03:28 +0300

    Print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* print_rev - updates the value of variable n using pointers
+* @s: int variable to be swapped
+* Return: Always 0
+*/
+
+void print_rev(char *s)
+{
+
+	char *start = s;
+
+	while (*start != '\0')
+	{
+		start++;
+
+	}
+	while (start != s)
+	{
+		start--;
+		_putchar(*start);
+	}
+	_putchar('\n');
+}
