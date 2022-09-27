a7abf0a000d0968ecb4b23e01c9664f7d0cb2f96
Rogers Ashaba <65979430+rogersashaba@users.noreply.github.com>
2022-09-27 22:11:38 +0300

    Print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_chessboard- fills memory with a constant byte
+* @a: 2D array of array
+* Return: void
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
