b4b3dddc6868adf49fe171d17764a4a1b92d8fa2
Rogers Ashaba <65979430+rogersashaba@users.noreply.github.com>
2022-09-28 23:39:05 +0300

    puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to ba printed
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+	_puts_recursion(s + 1);
+	}
+
+}
