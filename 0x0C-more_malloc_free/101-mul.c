0815141a8815792f011258441f5250921ddf0479
rogersashaba <rogersashaba2@gmail.com>
2022-10-06 14:43:29 -0700

    This is the mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,8 +8,8 @@
  */
 int main(int argc, char *argv[])
 {
-	unsigned long mul;
-	int i, j;
+unsigned long mul;
+int i, j;
 	if (argc != 3)
 	{
 		printf("Error\n");
@@ -27,6 +27,6 @@ int main(int argc, char *argv[])
 	}
 	mul = atol(argv[1]) * atol(argv[2]);
 	printf("%lu\n", mul);
-	return (0);
+return (0);
 }
 
