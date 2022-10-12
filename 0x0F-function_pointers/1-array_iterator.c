979072acd51b09ea39357190d29f49d16acee661
Rogers Ashaba <65979430+rogersashaba@users.noreply.github.com>
2022-10-12 23:27:27 +0300

    array_iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints each element of array
+ * @array: input interger array
+ * @size: size of the array
+ * @action: pointer to the function
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
